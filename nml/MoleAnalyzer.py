0d3d677bb7533ffa393057f4a13e6e06b7b4a5fd
ofir arzi <ofir.arzi@huji.mail.ac.il>
2016-09-16 08:42:48 +0300

    with mole analyze

--- nml/MoleAnalyzer.py
+++ nml/MoleAnalyzer.py
@@ -1,11 +1,14 @@
 from WorkingPys.K2means import extract_mole_grayscale
 from WorkingPys.SymetryTest import pic_symetry_test
 
+import os
 
 def analyze_mole(image_path):
     extract_mole_grayscale(imagepath=image_path,
                            imagename=r"C:\Users\ofir arzi\Desktop\NoMole\NoMole\nml\images\analyze\temp")
 
     symval = pic_symetry_test(r"C:\Users\ofir arzi\Desktop\NoMole\NoMole\nml\images\analyze\temp")
+
+    os.remove(r"C:\Users\ofir arzi\Desktop\NoMole\NoMole\nml\images\analyze\temp")
 
     return abs(symval - 91.21) > abs(symval - 104.75)
