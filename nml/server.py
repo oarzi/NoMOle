840b5e7fb7e81a383b4bb958eb64cb509e791763
oarzi <ofir.arzi@gmail.com>
2016-09-20 00:53:49 +0300

    pdf file (#3)

--- nml/server.py
+++ nml/server.py
@@ -25,4 +25,4 @@ def image():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7891))
-    app.run(host='192.168.0.52', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
